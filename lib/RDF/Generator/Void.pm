c9ec11ba2d00e97cc9f18ca63af9109b98692146
User <tope.omitola@googlemail.com>
2011-05-26 12:06:40 +0100

    commiting first code

---
+++ lib/RDF/Generator/Void.pm
@@ -0,0 +1,111 @@
+package RDF::Generator::Void;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+RDF::Generator::Void - The great new RDF::Generator::Void!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RDF::Generator::Void;
+
+    my $foo = RDF::Generator::Void->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Tope Omitola, C<< <tope.omitola at googlemail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rdf-generator-void at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RDF-Generator-Void>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RDF::Generator::Void
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RDF-Generator-Void>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RDF-Generator-Void>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RDF-Generator-Void>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RDF-Generator-Void/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Tope Omitola.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of RDF::Generator::Void
