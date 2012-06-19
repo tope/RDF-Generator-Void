07e6f8ed690ea7580461906a8d8066ed5ec071b7
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2012-06-19 23:53:01 +0200

    bump version

--- lib/RDF/Generator/Void.pm
+++ lib/RDF/Generator/Void.pm
@@ -22,7 +22,7 @@ RDF::Generator::Void - Generate VoID descriptions based on data in an RDF model
 
 =head1 VERSION
 
-Version 0.01_14
+Version 0.01_15
 
 Note that this is an early alpha release. It has pretty limited
 functionality, and there may very significant changes in this module
@@ -30,7 +30,7 @@ coming up really soon.
 
 =cut
 
-our $VERSION = '0.01_14';
+our $VERSION = '0.01_15';
 
 =head1 SYNOPSIS
 
