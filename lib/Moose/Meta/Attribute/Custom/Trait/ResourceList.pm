37ae782f342a2cddf96597a438ad387c961a7fbc
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2012-08-31 13:47:28 +0200

    Add resourcelist

---
+++ lib/Moose/Meta/Attribute/Custom/Trait/ResourceList.pm
@@ -0,0 +1,15 @@
+package Moose::Meta::Attribute::Custom::Trait::ResourceList;
+use Moose::Role;
+sub register_implementation {'RDF::Generator::Void::KjetilTrait'};
+
+=head2 NAME
+
+Moose::Meta::Attribute::Custom::Trait::ResourceList - Trait for lists of resources
+
+=head1 DESCRIPTION
+
+See L<RDF::Generator::Void::Meta::Attribute::ResourceList> for documentation.
+
+=cut
+
+1;
