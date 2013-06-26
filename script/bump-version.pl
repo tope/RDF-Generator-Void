b4fbcd7a2b59fc50a0a3430e7b3b81491e532219
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2013-06-27 00:47:44 +0200

    Bedtime

--- script/bump-version.pl
+++ script/bump-version.pl
@@ -4,11 +4,11 @@ use warnings;
 use strict;
 
 use RDF::Generator::Void;
-use File::Slurp qw( edit_file ) ;
+use File::Slurp qw( edit_file_lines ) ;
 
 my $oldver = $RDF::Generator::Void::VERSION;
 
 my $newver = $ARGV[0];
 
-edit_file { s/(version =?\s*) $oldver/$1 $newver/ig } 'lib/RDF/Generator/Void.pm' ;
+edit_file_lines { s/(version\s+\=?\s*)$oldver/$1$newver/ig } 'lib/RDF/Generator/Void.pm' ;
 
