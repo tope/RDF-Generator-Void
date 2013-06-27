e500e78500f869f694017c2ab3602f12de19b1cb
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2013-06-27 14:41:34 +0200

    Add a script to ease bumping version

--- script/bump-version.pl
+++ script/bump-version.pl
@@ -10,5 +10,15 @@ my $oldver = $RDF::Generator::Void::VERSION;
 
 my $newver = $ARGV[0];
 
-edit_file_lines { s/(version\s+\=?\s*)$oldver/$1$newver/ig } 'lib/RDF/Generator/Void.pm' ;
+edit_file_lines { s/(version\s+\=?\s*'?)$oldver('?)/$1$newver$2/ig } 'lib/RDF/Generator/Void.pm' ;
 
+edit_file_lines { s/(Version) $oldver/$1 $newver/g } 't/data/basic-expected.ttl' ;
+
+(my $eoldver = $oldver) =~ s/\./-/;
+(my $enewver = $newver) =~ s/\./-/;
+
+edit_file_lines { s/(v_)$eoldver/$1$enewver/g } 't/data/basic-expected.ttl' ;
+
+edit_file_lines { s/(my:project :release)/$1 my:v_$enewver,/ } 'meta/changes.ttl' ;
+
+1;
